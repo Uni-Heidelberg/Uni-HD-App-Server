7864687a73e67125befb34c12a696fa6af64e0fe
Christian Kohlstedde <christian@kohlsted.de>
2014-11-21 20:16:31 +0100

    updating vagrant configs and provisioning

--- Vagrantfile
+++ Vagrantfile
@@ -6,8 +6,7 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "chrko/debian-web"
 
-  config.vm.network "forwarded_port", guest: 3000, host: 3000
-  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 80, host: 8080, auto_correct: true
 
   config.vm.network "public_network"
 
@@ -16,5 +15,9 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     vb.cpus = 2
   end
 
-  config.vm.provision "shell", path: "provision.sh", privileged: false
+  config.vm.provision "shell", inline: "apt-get update && apt-get dist-upgrade -y", run: "always"
+  config.vm.provision "shell", path: "vagrant-configs/provision.sh", privileged: false
+  config.vm.provision "shell",
+        inline: "cd /vagrant; npm install; forever start server/server.js",
+        run: "always", privileged: false
 end
--- vagrant-configs/provision.sh
+++ vagrant-configs/provision.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-echo "deb http://http.debian.net/debian wheezy-backports main" | sudo tee /etc/apt/sourcesvag   .list.d/backports.list
+echo "deb http://http.debian.net/debian wheezy-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
 echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 echo 'mysql-server mysql-server/root_password password appserver' | sudo debconf-set-selections
 echo 'mysql-server mysql-server/root_password_again password appserver' | sudo debconf-set-selections
-sudo apt-get -y install git-core curl build-essential openssl libssl-dev mongodb-org
+sudo apt-get -y install git-core curl build-essential openssl libssl-dev
+sudo apt-get -t wheezy-backports install mongodb
 sudo chown -R $USER /usr/local
 cd
 git clone https://github.com/joyent/node.git
@@ -25,4 +26,9 @@ make
 make install
 cd
 npm install -g strongloop
+npm install -g forever
 echo "CREATE USER 'appserver'@'localhost' IDENTIFIED BY 'appserver';GRANT USAGE ON * . * TO 'appserver'@'localhost' IDENTIFIED BY 'appserver';CREATE DATABASE IF NOT EXISTS appserver;GRANT ALL PRIVILEGES ON appserver . * TO 'appserver'@'localhost';" | mysql -u root -pvagrant
+sudo cp /vagrant/vagrant-configs/default /etc/apache2/sites-available/default
+sudo cp /vagrant/vagrant-configs/phpmyadmin-secure.conf /etc/apache2/conf.d/phpmyadmin-secure.conf
+sudo a2enmod proxy_http
+sudo service apache2 restart
