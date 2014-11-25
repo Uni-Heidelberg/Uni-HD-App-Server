b112d8e67f8280a08a7337f6297a29f5f8b0e3b9
Christian Kohlstedde <christian@kohlsted.de>
2014-11-25 22:36:59 +0100

    fixing bug @ provision.sh

--- vagrant-configs/provision.sh
+++ vagrant-configs/provision.sh
@@ -6,7 +6,7 @@ sudo apt-get dist-upgrade -y
 echo 'mysql-server mysql-server/root_password password appserver' | sudo debconf-set-selections
 echo 'mysql-server mysql-server/root_password_again password appserver' | sudo debconf-set-selections
 sudo apt-get -y install git-core curl build-essential openssl libssl-dev
-sudo apt-get -t wheezy-backports install mongodb
+sudo apt-get -y -t wheezy-backports install mongodb
 sudo chown -R $USER /usr/local
 cd
 git clone https://github.com/joyent/node.git
@@ -27,6 +27,6 @@ npm install -g strongloop
 npm install -g forever
 echo "CREATE USER 'appserver'@'localhost' IDENTIFIED BY 'appserver';GRANT USAGE ON * . * TO 'appserver'@'localhost' IDENTIFIED BY 'appserver';CREATE DATABASE IF NOT EXISTS appserver;GRANT ALL PRIVILEGES ON appserver . * TO 'appserver'@'localhost';" | mysql -u root -pvagrant
 sudo cp /vagrant/vagrant-configs/default /etc/apache2/sites-available/default
-sudo cp /vagrant/vagrant-configs/phpmyadmin-secure.conf /etc/apache2/conf.d/phpmyadmin-secure.conf
+#sudo cp /vagrant/vagrant-configs/phpmyadmin-secure.conf /etc/apache2/conf.d/phpmyadmin-secure.conf
 sudo a2enmod proxy_http
 sudo service apache2 restart
